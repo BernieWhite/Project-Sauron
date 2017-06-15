24a53d889c189e32139e0129620f039f1217f260
Bernie White <bewhite@microsoft.com>
2017-06-15 23:33:30 +1000

    Update to automatically generate .dll file

--- Create-Manifest.ps1
+++ Create-Manifest.ps1
@@ -69,76 +69,127 @@ $CustomEventsMAN = "$BasePathName.man"				# The Manifest file
 $CustomEvents = Import-CSV $InputFile
 $Providers = $CustomEvents | Select-Object -Property ProviderSymbol,ProviderName,ProviderGuid -Unique # Extract the provider information from input
 
-# Create The Manifest XML Document
-$XmlWriter = New-Object System.XMl.XmlTextWriter($CustomEventsMAN,$null)
+try {
+  # Create The Manifest XML Document
+  $XmlWriter = New-Object System.XMl.XmlTextWriter($CustomEventsMAN,$null)
 
-# Set The Formatting
-$xmlWriter.Formatting = "Indented"
-$xmlWriter.Indentation = "4"
+  # Set The Formatting
+  $xmlWriter.Formatting = "Indented"
+  $xmlWriter.Indentation = "4"
 
-# Write the XML Decleration
-$xmlWriter.WriteStartDocument()
+  # Write the XML Decleration
+  $xmlWriter.WriteStartDocument()
 
-# Create Instrumentation Manifest
-$xmlWriter.WriteStartElement("instrumentationManifest")
-$xmlWriter.WriteAttributeString("xsi:schemaLocation","http://schemas.microsoft.com/win/2004/08/events eventman.xsd")
-$xmlWriter.WriteAttributeString("xmlns","http://schemas.microsoft.com/win/2004/08/events")
-$xmlWriter.WriteAttributeString("xmlns:win","http://manifests.microsoft.com/win/2004/08/windows/events")
-$xmlWriter.WriteAttributeString("xmlns:xsi","http://www.w3.org/2001/XMLSchema-instance")
-$xmlWriter.WriteAttributeString("xmlns:xs","http://www.w3.org/2001/XMLSchema")
-$xmlWriter.WriteAttributeString("xmlns:trace","http://schemas.microsoft.com/win/2004/08/events/trace")
+  # Create Instrumentation Manifest
+  $xmlWriter.WriteStartElement("instrumentationManifest")
+  $xmlWriter.WriteAttributeString("xsi:schemaLocation","http://schemas.microsoft.com/win/2004/08/events eventman.xsd")
+  $xmlWriter.WriteAttributeString("xmlns","http://schemas.microsoft.com/win/2004/08/events")
+  $xmlWriter.WriteAttributeString("xmlns:win","http://manifests.microsoft.com/win/2004/08/windows/events")
+  $xmlWriter.WriteAttributeString("xmlns:xsi","http://www.w3.org/2001/XMLSchema-instance")
+  $xmlWriter.WriteAttributeString("xmlns:xs","http://www.w3.org/2001/XMLSchema")
+  $xmlWriter.WriteAttributeString("xmlns:trace","http://schemas.microsoft.com/win/2004/08/events/trace")
 
-# Create Instrumentation, Events and Provider Elements
-$xmlWriter.WriteStartElement("instrumentation")
-	$xmlWriter.WriteStartElement("events")
-	
-	$Providers = $CustomEvents | Select-Object -Property ProviderSymbol,ProviderName,ProviderGuid -Unique
-	ForEach($Provider in $Providers){
-		$xmlWriter.WriteStartElement("provider")
-			$xmlWriter.WriteAttributeString("name",($Provider.ProviderName))
-			$xmlWriter.WriteAttributeString("guid",$Provider.ProviderGUID)
-			$xmlWriter.WriteAttributeString("symbol",$Provider.ProviderSymbol)
-			$xmlWriter.WriteAttributeString("resourceFileName",$CustomEventsDLL)
-			$xmlWriter.WriteAttributeString("messageFileName",$CustomEventsDLL)
-			$xmlWriter.WriteAttributeString("parameterFileName",$CustomEventsDLL)
-			$xmlWriter.WriteStartElement("channels")
+  # Create Instrumentation, Events and Provider Elements
+  $xmlWriter.WriteStartElement("instrumentation")
+    $xmlWriter.WriteStartElement("events")
+    
+    $Providers = $CustomEvents | Select-Object -Property ProviderSymbol,ProviderName,ProviderGuid -Unique
+    ForEach($Provider in $Providers){
+      $xmlWriter.WriteStartElement("provider")
+        $xmlWriter.WriteAttributeString("name",($Provider.ProviderName))
+        $xmlWriter.WriteAttributeString("guid",$Provider.ProviderGUID)
+        $xmlWriter.WriteAttributeString("symbol",$Provider.ProviderSymbol)
+        $xmlWriter.WriteAttributeString("resourceFileName",$CustomEventsDLL)
+        $xmlWriter.WriteAttributeString("messageFileName",$CustomEventsDLL)
+        $xmlWriter.WriteAttributeString("parameterFileName",$CustomEventsDLL)
+        $xmlWriter.WriteStartElement("channels")
 
-			$Channels = $CustomEvents | Where-Object{$_.ProviderSymbol -eq $Provider.ProviderSymbol}
-			ForEach($Channel in $Channels){	
-				$xmlWriter.WriteStartElement("channel")	
-					$xmlWriter.WriteAttributeString("name",$Channel.ChannelName)
-					$xmlWriter.WriteAttributeString("chid",($Channel.ChannelName).Replace(' ',''))
-					$xmlWriter.WriteAttributeString("symbol",$Channel.ChannelSymbol)
-					$xmlWriter.WriteAttributeString("type","Admin")
-					$xmlWriter.WriteAttributeString("enabled","false")
-				$xmlWriter.WriteEndElement() # Closing channel
-				}
-			$xmlWriter.WriteEndElement() # Closing channels
-		$xmlWriter.WriteEndElement() # Closing provider
-	}		
-	$xmlWriter.WriteEndElement() # Closing events
-$xmlWriter.WriteEndElement() # Closing Instrumentation
-$xmlWriter.WriteEndElement()   # Closing instrumentationManifest
+        $Channels = $CustomEvents | Where-Object{$_.ProviderSymbol -eq $Provider.ProviderSymbol}
+        ForEach($Channel in $Channels){	
+          $xmlWriter.WriteStartElement("channel")	
+            $xmlWriter.WriteAttributeString("name",$Channel.ChannelName)
+            $xmlWriter.WriteAttributeString("chid",($Channel.ChannelName).Replace(' ',''))
+            $xmlWriter.WriteAttributeString("symbol",$Channel.ChannelSymbol)
+            $xmlWriter.WriteAttributeString("type","Admin")
+            $xmlWriter.WriteAttributeString("enabled","false")
+          $xmlWriter.WriteEndElement() # Closing channel
+          }
+        $xmlWriter.WriteEndElement() # Closing channels
+      $xmlWriter.WriteEndElement() # Closing provider
+    }		
+    $xmlWriter.WriteEndElement() # Closing events
+  $xmlWriter.WriteEndElement() # Closing Instrumentation
+  $xmlWriter.WriteEndElement()   # Closing instrumentationManifest
 
-# End the XML Document
-$xmlWriter.WriteEndDocument()
+  # End the XML Document
+  $xmlWriter.WriteEndDocument()
 
-# Finish The Document
-$xmlWriter.Finalize
-$xmlWriter.Flush()
-$xmlWriter.Close()
+  $xmlWriter.Flush()
+}
+finally {
+  # Close the writer
+  $xmlWriter.Close();
+  $XmlWriter.Dispose();
+}
+
+function InvokeBuildTool {
+
+  [CmdletBinding()]
+  param (
+    [Parameter(Mandatory = $True)]
+    [String]$Command,
+
+    [Parameter(Mandatory = $False)]
+    [String[]]$Parameters = $Null
+  )
+
+  process {
+    
+    $process = New-Object -TypeName System.Diagnostics.Process;
+    $process.StartInfo.FileName = $Command;
+    $process.StartInfo.WindowStyle =  [System.Diagnostics.ProcessWindowStyle]::Hidden;
+    $process.StartInfo.WorkingDirectory = $PWD;
+
+    if ($Null -ne $Parameters -and $Parameters.Count -gt 0) {
+      $process.StartInfo.Arguments = [String]::Join(' ', $Parameters);
+    }
+
+    Write-Verbose -Message "About to call: $Command";
+    Write-Verbose -Message "With Parameters: $($process.StartInfo.Arguments)";
+
+    $process.Start() | Out-Null;
+    $process.WaitForExit();
+  }
+}
+
+InvokeBuildTool -Command 'C:\Program Files (x86)\Windows Kits\10\bin\x64\mc.exe' -Parameters @(
+  '-css',
+  '"Namespace"',
+  "`"$CustomEventsMAN`""
+)
+
+InvokeBuildTool -Command 'C:\Program Files (x86)\Windows Kits\10\bin\x64\rc.exe' -Parameters "`"$BasePathName.rc`"";
+
+InvokeBuildTool -Command 'C:\Windows\Microsoft.NET\Framework64\v4.0.30319\csc.exe' -Parameters @(
+  "/win32res:`"$BasePathName.res`"",
+  '/unsafe',
+  '/target:library',
+  "/out:`"$BasePathName.dll`"",
+  "`"$BasePathName.cs`""
+)
+
+# Cleanup temp files
+Remove-Item -Path @(
+  "$BasePathName.rc",
+  "$BasePathName.res",
+  "$BasePathName.cs",
+  "$($BasePathName)TEMP.BIN"
+)
 
 # Output the usage instructions
 Write-Host "`nThe manifest file has been generated at `"$CustomEventsMAN`"`n"
-Write-Host "Step 1: With the Windows 10 SDK installed, open a Command Prompt and change directory to the folder with the .man file (This will not work in PowerShell!) `n"
-
-Write-Host "`t `"C:\Program Files (x86)\Windows Kits\10\bin\x64\mc.exe`" `"$CustomEventsMAN`""
-Write-Host "`t `"C:\Program Files (x86)\Windows Kits\10\bin\x64\mc.exe`" -css `"NameSpace`" `"$CustomEventsMAN`""
-Write-Host "`t `"C:\Program Files (x86)\Windows Kits\10\bin\x64\rc.exe`" `"$BasePathName.rc`""
-Write-Host "`t `"C:\Windows\Microsoft.NET\Framework64\v4.0.30319\csc.exe`" /win32res:`"$BasePathName.res`" /unsafe /target:library /out:`"$BasePathName.dll`" `"$BasePathName.cs`"`n"
-
-Write-Host "Step 2: On the WEC server, copy both the .man and .dll file to $DLLPath"
-Write-Host "Step 3: Load the custom event channels by executing:`n"
+Write-Host "Step 1: On the WEC server, copy both the .man and .dll file to $DLLPath"
+Write-Host "Step 2: Load the custom event channels by executing:`n"
 Write-Host "`t `"c:\windows\system32\wevtutil.exe`" im `"$DLLPath\$BaseName.man`"`n"
 
 # -----------------------------------------------------------------------------------
